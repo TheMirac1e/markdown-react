db43d7a3cb85dd7c5fca92ee69896df950cfad8f
Even Koshliak <evenkoshliak@o-s-i.org>
2022-11-20 22:39:36 +0200

    HTML/CSS/JS - add simple markdown

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {Component, useState} from 'react'
+import ReactMarkdown from 'react-markdown'
 import './App.scss'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface S {
+    [key: string] : string
+}
 
-  return (
-    <div className="App">
-        laksjdf
-    </div>
-  )
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            areaValue: null,
+        }
+    }
+
+    onChangeEvent = (e: any) => {
+        this.setState({
+            areaValue: e.target.value
+        })
+    }
+
+    render() {
+        const { areaValue }: Readonly<S> = this.state;
+        return (
+            <div className="App">
+                <textarea className={"textarea"} name="markdown" onChange={(e) => this.onChangeEvent(e)} id="editor"></textarea>
+                <ReactMarkdown children={areaValue}/>
+            </div>
+        )
+    }
 }
 
 export default App
